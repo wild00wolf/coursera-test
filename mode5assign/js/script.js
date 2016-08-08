d693ba5346502206998c3f5063c0e8017568beae
Maggie Hon <color_hon@hotmail.com>
2016-08-08 11:43:17 +0800

    Assignment Upadted Changes

--- mode5assign/js/script.js
+++ mode5assign/js/script.js
@@ -119,13 +119,13 @@ function buildAndShowHomeHTML (categories) {
       // 
       // var homeHtmlToInsertIntoMainPage = ....
 
-        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName+ "'");
     
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that. 
       // ....
-         document.querySelector("#main-content").innerHTML = homeHtml;
+         insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
